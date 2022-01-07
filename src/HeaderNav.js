5a50ae307c69f4e599659726b176a51bf046c4d2
Jenny Kim <kimj1905@gmail.com>
2022-01-07 17:27:20 -0500

    added css to logo on header

--- src/HeaderNav.js
+++ src/HeaderNav.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeaderNav() {
 	return (
 		<div className="title-text header-nav">
-			<span>jennykim.</span>
+			<span className="logo">jennykim.</span>
 			<ul className="sections">
 				<li>
 					<a href="#aboutme">About Me</a>

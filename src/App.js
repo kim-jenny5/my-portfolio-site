0bdbfa9fe82cd15fa25ebbb843f2a4424cd25d3f
Jenny Kim <kimj1905@gmail.com>
2022-01-07 17:02:45 -0500

    added blobs onto page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-function App() {
+export default function App() {
 	return (
 		<div className="App">
-			<header className="App-header">Hello</header>
+			{/* <div className="blob-1">
+				<div className="vector">1</div>
+			</div>
+			<div className="blob-2">
+				<div className="vector"></div>
+			</div> */}
+			<div className="blob-1">
+				<svg
+					width="219"
+					height="589"
+					viewBox="0 0 219 589"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M-172.91 588.291C-236.659 573.844 -289.713 533.669 -327.687 480.466C-374.513 414.861 -428.811 338.855 -406.16 261.502C-383.11 182.783 -297.158 143.921 -220.554 114.602C-151.672 88.2381 -73.2048 74.9699 -7.78477 109.028C51.9282 140.115 65.2328 214.197 90.5625 276.57C113.201 332.316 148.978 389.132 129.015 445.89C109.119 502.457 47.4979 527.624 -6.73626 553.203C-59.9654 578.308 -115.513 601.299 -172.91 588.291Z"
+						fill="#EAE3D7"
+					/>
+				</svg>
+			</div>
+			<div className="blob-2">
+				<svg
+					width="653"
+					height="695"
+					viewBox="0 0 653 695"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill-rule="evenodd"
+						clip-rule="evenodd"
+						d="M241.759 -7.47232C312.054 -33.7167 385.313 -6.74578 457.123 15.0133C531.819 37.647 612.917 54.1606 657.221 118.417C706.771 190.283 722.464 282.479 699.625 366.73C675.613 455.311 617.306 535.029 533.902 573.329C452.778 610.581 356.808 600.145 276.583 560.993C205.457 526.281 173.438 450.352 137.462 379.856C101.154 308.71 50.6711 236.136 71.4395 159.007C92.8983 79.3153 164.441 21.3941 241.759 -7.47232Z"
+						fill="#EBCCC1"
+					/>
+				</svg>
+			</div>
 		</div>
 	);
 }
-
-export default App;

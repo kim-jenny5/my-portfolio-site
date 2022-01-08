8108f825638a4cb5c5496270b7c292c0e8ba20bd
Jenny Kim <kimj1905@gmail.com>
2022-01-08 17:57:11 -0500

    centered introduction container

--- src/Introduction.js
+++ src/Introduction.js
@@ -3,11 +3,13 @@ import React from "react";
 export default function Introduction() {
 	return (
 		<div className="introduction">
-			<div className="title">
-				<div>Hi, my name is Jenny.</div>
-				<div>Nice to meet you!</div>
+			<div className="text-container">
+				<div className="title">
+					<div>Hi, my name is Jenny.</div>
+					<div>Nice to meet you!</div>
+				</div>
+				<div className="title-text subtitle">I'm a frontend developer.</div>
 			</div>
-			<div className="title-text subtitle">I'm a frontend developer.</div>
 			<div className="picture"></div>
 		</div>
 	);

6dd7aee8bff3c1c63952faad5da6fe157ed752fc
Jenny Kim <kimj1905@gmail.com>
2022-01-08 17:42:19 -0500

    added introduction html and preliminary css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import BlobGraphics from "./BlobGraphics";
 import HeaderNav from "./HeaderNav";
+import Introduction from "./Introduction";
 
 export default function App() {
 	return (
 		<div className="App">
 			<BlobGraphics />
 			<HeaderNav />
+			<Introduction />
 		</div>
 	);
 }
---
+++ src/Introduction.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Introduction() {
+	return (
+		<div className="introduction">
+			<div className="title">
+				<div>Hi, my name is Jenny.</div>
+				<div>Nice to meet you!</div>
+			</div>
+			<div className="title-text subtitle">I'm a frontend developer.</div>
+		</div>
+	);
+}

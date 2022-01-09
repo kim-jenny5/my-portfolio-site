d118cdd0d90efb0395761f173977ed5d953e455f
Jenny Kim <kimj1905@gmail.com>
2022-01-08 20:33:35 -0500

    started on projects section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BlobGraphics from "./BlobGraphics";
 import HeaderNav from "./HeaderNav";
 import Introduction from "./Introduction";
 import About from "./About";
+import Projects from "./Projects";
 
 export default function App() {
 	return (
@@ -10,6 +11,7 @@ export default function App() {
 			<HeaderNav />
 			<Introduction />
 			<About />
+			<Projects />
 		</div>
 	);
 }

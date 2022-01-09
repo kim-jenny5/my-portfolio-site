192eeb396b9e18227436ffd0c07ed12e05f0d3d5
Jenny Kim <kimj1905@gmail.com>
2022-01-08 19:49:49 -0500

    added picture on about seciton

--- src/About.js
+++ src/About.js
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function About() {
 	return (
-		<div id="aboutme" className="title-text">
-			About Me
+		<div id="aboutme">
+			<div className="container">
+				<div className="description">
+					<div className="picture"></div>
+					<div className="title-text">About Me</div>
+					<hr></hr>
+				</div>
+				<div className="tech-stack"></div>
+			</div>
 		</div>
 	);
 }

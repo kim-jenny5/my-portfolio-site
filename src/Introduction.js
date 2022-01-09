30f4f3a069dbf90c73b7c3df7bc60f00c293c599
Jenny Kim <kimj1905@gmail.com>
2022-01-08 19:15:06 -0500

    configured react-typical

--- src/Introduction.js
+++ src/Introduction.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
+import Typical from "react-typical";
 
-export default function Introduction() {
-	return (
-		<div className="introduction">
-			<div className="text-container">
-				<div className="title">
-					<div>Hi, my name is Jenny.</div>
-					<div>Nice to meet you!</div>
+export default class Introduction extends Component {
+	steps = [
+		"a frontend developer",
+		1500,
+		"a creator",
+		1500,
+		"detail-oriented",
+		1500,
+		"analytical",
+		1500,
+		"culturally intelligent",
+		1500,
+		"always curious",
+		1500
+	];
+
+	render() {
+		return (
+			<div className="introduction">
+				<div className="text-container">
+					<div className="title">
+						<div>Hi, my name is Jenny.</div>
+						<div>Nice to meet you!</div>
+					</div>
+					<div className="title-text subtitle">
+						I am <Typical wrapper="span" steps={this.steps} loop={Infinity} />
+					</div>
 				</div>
-				<div className="title-text subtitle">I'm a frontend developer.</div>
+				<div className="picture"></div>
 			</div>
-			<div className="picture"></div>
-		</div>
-	);
+		);
+	}
 }

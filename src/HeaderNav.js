6fe0973b166e663db1a5b6a045426fa2673f26b5
Jenny Kim <kimj1905@gmail.com>
2022-01-09 23:50:47 -0500

    added scroll feature to scroll all the way back up to the top once logo is clicked

--- src/HeaderNav.js
+++ src/HeaderNav.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+const handleClick = (e) => {
+	window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
 export default function HeaderNav() {
 	return (
 		<div className="title-text header-nav">
 			{/* <span className="logo">jennykim.</span> */}
-			<button className="logo">jennykim.</button>
+			<button className="logo" onClick={handleClick}>
+				jennykim.
+			</button>
 			<ul>
 				<li>
 					<a href="#aboutme">About Me</a>

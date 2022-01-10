6373fa83225e7f1de92bfa65c86cef0c4383b808
Jenny Kim <kimj1905@gmail.com>
2022-01-10 16:11:31 -0500

    stylized resume educaiton and experience titles

--- src/Resume.js
+++ src/Resume.js
@@ -4,8 +4,18 @@ export default function Resume() {
 	return (
 		<div id="resume">
 			<div className="container">
-				<div className="title-text">
-					Resume<hr></hr>
+				<div className="top">
+					<div className="title-text">
+						Resume <hr></hr>
+					</div>
+					<div className="resume-details">
+						<div className="education">
+							<div className="title-text">Education</div>
+						</div>
+						<div className="experience">
+							<div className="title-text">Experience</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>

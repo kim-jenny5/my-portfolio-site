ba9fc1dacc5dad5fc4c686f7a284d5d9ea836335
Jenny Kim <kimj1905@gmail.com>
2022-01-10 16:37:58 -0500

    added resume experience section text html

--- src/Experience.js
+++ src/Experience.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function Experience(props) {
-	return <div></div>;
+	return (
+		<div className="each-experience">
+			<div>
+				<span>{props.name}</span>
+				<span>{props.city}</span>
+			</div>
+			<div>
+				<span>{props.position}</span>
+				<span>{props.dates}</span>
+			</div>
+			<div>
+				<ul>
+					{props.description.map((desc) => (
+						<li>{desc}</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }

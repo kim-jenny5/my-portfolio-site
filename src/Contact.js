5eef3db25b20f4b17eed0f7a2db4dc7619ac9f0f
Jenny Kim <kimj1905@gmail.com>
2022-01-10 18:38:41 -0500

    added links to icons on contact section

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Envelope, GithubFill, LinkedInV1Fill } from "akar-icons";
 
 export default function Contact() {
 	return (
@@ -12,7 +13,19 @@ export default function Contact() {
 			<div id="msg">
 				Please feel free to reach out to me through any of these platforms. 💌
 			</div>
-			<div id="contact-links"></div>
+			<div id="contact-links">
+				<span>
+					<a href="mailto:jennykimdev@gmail.com" target="_blank">
+						<Envelope strokeWidth={2} size={30} />
+					</a>
+					<a href="https://github.com/kim-jenny5" target="_blank">
+						<GithubFill strokeWidth={2} size={30} />
+					</a>
+					<a href="https://www.linkedin.com/in/kim-jenny5/" target="_blank">
+						<LinkedInV1Fill strokeWidth={2} size={30} />
+					</a>
+				</span>
+			</div>
 		</div>
 	);
 }

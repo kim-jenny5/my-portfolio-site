148f7a99802f1ef534f95a19f3712d04cefec15c
Jenny Kim <kimj1905@gmail.com>
2022-01-10 18:22:55 -0500

    added reach out message to contact section

--- src/Contact.js
+++ src/Contact.js
@@ -4,8 +4,15 @@ export default function Contact() {
 	return (
 		<div id="contact">
 			<div className="title-text">
-				Contact Me<hr></hr>
+				<div>
+					Contact Me<hr></hr>
+				</div>
+				<div>Let's work together!</div>
 			</div>
+			<div id="msg">
+				Please feel free to reach out to me through any of these platforms. 💌
+			</div>
+			<div id="contact-links"></div>
 		</div>
 	);
 }

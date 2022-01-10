53a178079ecee7319586333ba3e574068c1b9d62
Jenny Kim <kimj1905@gmail.com>
2022-01-10 16:24:50 -0500

    changed class components to function components for faster optimization

--- src/Introduction.js
+++ src/Introduction.js
@@ -1,44 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import Typical from "react-typical";
 import IMG_1818 from "./images/IMG_1818.PNG";
 
-export default class Introduction extends Component {
-	steps = [
-		"a frontend developer.",
-		1500,
-		"a creator.",
-		1500,
-		"detail-oriented.",
-		1500,
-		"analytical.",
-		1500,
-		"culturally intelligent.",
-		1500,
-		"always curious.",
-		1500
-	];
+const steps = [
+	"a frontend developer.",
+	1500,
+	"a creator.",
+	1500,
+	"detail-oriented.",
+	1500,
+	"analytical.",
+	1500,
+	"culturally intelligent.",
+	1500,
+	"always curious.",
+	1500
+];
 
-	render() {
-		return (
-			<div className="introduction">
-				<div className="text-container">
-					<div className="title">
-						<div>Hi, my name is Jenny.</div>
-						<div>Nice to meet you!</div>
-					</div>
-					<div className="title-text subtitle">
-						I am{" "}
-						<Typical
-							wrapper="span"
-							steps={this.steps}
-							loop={Infinity}
-							className="typing"
-						/>
-					</div>
+export default function Introduction() {
+	return (
+		<div className="introduction">
+			<div className="text-container">
+				<div className="title">
+					<div>Hi, my name is Jenny.</div>
+					<div>Nice to meet you!</div>
 				</div>
-				<img src={IMG_1818} className="picture"></img>
-				{/* <div className="picture"></div> */}
+				<div className="title-text subtitle">
+					I am{" "}
+					<Typical
+						wrapper="span"
+						steps={steps}
+						loop={Infinity}
+						className="typing"
+					/>
+				</div>
 			</div>
-		);
-	}
+			<img src={IMG_1818} className="picture"></img>
+			{/* <div className="picture"></div> */}
+		</div>
+	);
 }

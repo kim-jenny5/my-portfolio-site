649b141698f9d22dd6b198744ad73dfdb1e2e704
Jenny Kim <kimj1905@gmail.com>
2022-01-11 21:30:52 -0500

    converted blob px for responsiveness

--- src/BlobGraphics.js
+++ src/BlobGraphics.js
@@ -1,13 +1,13 @@
 import React from "react";
-import blob_1 from "./images/blob_1.png";
-import blob_2 from "./images/blob_2.png";
+// import blob_1 from "./images/blob_1.png";
+// import blob_2 from "./images/blob_2.png";
+import blob_1 from "./images/blob_1_frame.png";
+import blob_2 from "./images/blob_2_frame.png";
 
 export default function BlobGraphics() {
 	return (
 		<div>
-			{/* <div className="blob-1"></div> */}
 			<img src={blob_1} className="blob-1"></img>
-			{/* <div className="blob-2"></div> */}
 			<img src={blob_2} className="blob-2"></img>
 		</div>
 	);

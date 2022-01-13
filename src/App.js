c2653f15cc6c65d82f0e5fa0a4af82072f33f641
Jenny Kim <kimj1905@gmail.com>
2022-01-12 21:58:01 -0500

    removed margins from body with globalstyle

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
 import BlobGraphics from "./BlobGraphics";
 import HeaderNav from "./HeaderNav";
 import Introduction from "./Introduction";
@@ -10,14 +13,44 @@ import Footer from "./Footer";
 export default function App() {
 	return (
 		<div className="App">
-			<BlobGraphics />
-			<HeaderNav />
-			<Introduction />
-			<About />
-			<ProjectsCont />
-			<ResumeCont />
-			<Contact />
-			<Footer />
+			<GlobalStyle />
+			<Container>
+				<BlobGraphics />
+				<HeaderNav />
+				<Introduction />
+				{/* <About />
+				<ProjectsCont />
+				<ResumeCont />
+				<Contact />
+				<Footer /> */}
+			</Container>
 		</div>
 	);
 }
+
+const Container = styled.div`
+	display: grid;
+	grid-template-columns: repeat(12, 1fr);
+	grid-column-gap: 1.25rem;
+`;
+
+const GlobalStyle = createGlobalStyle`body {
+  margin: 0;
+  padding: 0;
+}`;
+
+// grid-template-areas: "header header header header" "main main main main" "footer footer footer footer";
+// grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+// position: relative;
+// align-items: center;
+// grid-template-columns: repeat(12, 1fr);
+// grid-template-columns: repeat(12, 8.33%);
+// margin: 0 70px 0 70px;
+// margin: 0;
+// padding: 0;
+
+// grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+// const Columns = styled.div`
+// 	width: 8.33%;
+// 	float: left;
+// `;

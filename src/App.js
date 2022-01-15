63cf59a4e05cb5d6798faf5951062ed79de7b863
Jenny Kim <kimj1905@gmail.com>
2022-01-15 00:03:44 -0500

    deleted unnecessary code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Footer from "./Components/03-Footer/Footer";
 // import ProjectsCont from "./ProjectsCont";
 // import ResumeCont from "./ResumeCont";
 // import Contact from "./Contact";
-// import Footer from "./Footer";/
 
 export default function App() {
 	return (
@@ -27,10 +26,7 @@ export default function App() {
 					<Contact /> */}
 				</Content>
 			</GridWrapper>
-			{/* <Ft> */}
 			<Footer />
-			{/* Footer
-			</Ft> */}
 		</div>
 	);
 }
@@ -132,16 +128,4 @@ export const Content = styled.div`
 	overflow: auto; */
 	background-color: lightpink;
 	/* width: 100vw; */
-`;
-
-export const Ft = styled.div`
-	grid-area: ft;
-	/* grid-row: 3;
-	grid-template-columns: 1;
-	font-size: 0.75em;
-	background: #eae3d7;
-	text-align: center;
-	padding: 2.125em 0 2.125em 0; */
-	/* background-color: lightblue; */
-	/* padding: 3.9260969976905313vh 0 3.9260969976905313vh 0; */
 `;

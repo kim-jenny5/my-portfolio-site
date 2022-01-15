9958480a74b7a408a3a627abe309a1a831dad983
Jenny Kim <kimj1905@gmail.com>
2022-01-14 19:05:36 -0500

    save

--- src/Components/01-Header/Header.js
+++ src/Components/01-Header/Header.js
@@ -1,12 +1,8 @@
-import React from "react";
+// import React from "react";
+import React, { Component } from "react";
 import { HeaderCont, Button, NavBar } from "./style";
 import { ThreeLineHorizontal } from "akar-icons";
 // import { HeaderCont } from "./App";
-
-const handleClick = () => {
-	window.scrollTo({ top: 0, behavior: "smooth" });
-	window.location.href = "";
-};
 
 // const NavBarComp = () => {
 // 	return (
@@ -27,28 +23,65 @@ const handleClick = () => {
 // 	);
 // };
 
-export default function Header() {
-	return (
-		<HeaderCont>
-			<Button onClick={handleClick}>jennykim.</Button>
-			{window.innerWidth >= "1200" ? (
-				<NavBar>
-					<li>
-						<a href="#aboutme">About Me</a>
-					</li>
-					<li>
-						<a href="#projects">Projects</a>
-					</li>
-					<li>
-						<a href="#resume">Resume</a>
-					</li>
-					<li>
-						<a href="#contact">Contact</a>
-					</li>
-				</NavBar>
-			) : (
-				<ThreeLineHorizontal strokeWidth={2} size={24} />
-			)}
-		</HeaderCont>
-	);
+// export default function Header() {
+// 	return (
+// 		<HeaderCont>
+// 			<Button onClick={handleClick}>jennykim.</Button>
+// 			{window.innerWidth >= "1200" ? (
+// 				<NavBar>
+// 					<li>
+// 						<a href="#aboutme">About Me</a>
+// 					</li>
+// 					<li>
+// 						<a href="#projects">Projects</a>
+// 					</li>
+// 					<li>
+// 						<a href="#resume">Resume</a>
+// 					</li>
+// 					<li>
+// 						<a href="#contact">Contact</a>
+// 					</li>
+// 				</NavBar>
+// 			) : (
+// 				<ThreeLineHorizontal strokeWidth={2} size={24} />
+// 			)}
+// 		</HeaderCont>
+// 	);
+// }
+
+export default class Header extends Component {
+	state = {
+		device: ""
+	};
+
+	handleClick = () => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+		window.location.href = "";
+	};
+
+	render() {
+		return (
+			<HeaderCont>
+				<Button onClick={this.handleClick}>jennykim.</Button>
+				{window.innerWidth >= "1200" ? (
+					<NavBar>
+						<li>
+							<a href="#aboutme">About Me</a>
+						</li>
+						<li>
+							<a href="#projects">Projects</a>
+						</li>
+						<li>
+							<a href="#resume">Resume</a>
+						</li>
+						<li>
+							<a href="#contact">Contact</a>
+						</li>
+					</NavBar>
+				) : (
+					<ThreeLineHorizontal strokeWidth={2} size={24} />
+				)}
+			</HeaderCont>
+		);
+	}
 }

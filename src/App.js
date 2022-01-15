ba632d8268c07873b19ea5df5b31915248160daa
Jenny Kim <kimj1905@gmail.com>
2022-01-15 14:54:36 -0500

    changed header into flexbox

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ const Wrapper = styled.div`
 	/* min-height: 100%; */
 
 	/* XS / Mobile Portrait */
-	@media only screen and (min-width: 320px) {
+	/* @media only screen and (min-width: 320px) {
 		grid-template-columns: repeat(4, 1fr);
 		grid-template-rows: auto 1fr 1fr auto;
 		grid-column-gap: 1.25rem;
@@ -103,77 +103,64 @@ const Wrapper = styled.div`
 
 		grid-template-areas:
 			"hd hd hd hd"
-			/* "content content content content" */
 			"intro intro intro intro"
 			"about about about about"
 			"ft ft ft ft";
-	}
+	} */
 
 	/* S / Mobile Landscape */
-	@media only screen and (min-width: 480px) {
+	/* @media only screen and (min-width: 480px) {
 		grid-template-columns: repeat(6, 1fr);
 		grid-template-rows: auto 1fr 1fr auto;
 		grid-column-gap: 1.25rem;
-		/* margin: 32.5px 32.5px 0 32.5px; */
 		margin: 2.031rem 2.031rem 0 2.031rem;
 
 		grid-template-areas:
 			"hd hd hd hd hd hd"
-			/* "content content content content content content" */
 			"intro intro intro intro intro intro"
 			"about about about about about about"
 			"ft ft ft ft ft ft";
-	}
+	} */
 
 	/* M / Tablet Portrait */
-	@media only screen and (min-width: 768px) {
+	/* @media only screen and (min-width: 768px) {
 		grid-template-columns: repeat(8, 1fr);
 		grid-template-rows: auto 1fr 1fr auto;
-		/* margin: 45px 45px 0 45px; */
 		margin: 2.813rem 2.813rem 0 2.813rem;
-		/* margin: 0 2.813rem 0 2.813rem; */
 
-		grid-template-areas:
+    grid-template-areas:
 			"hd hd hd hd hd hd hd hd"
-			/* "content content content content content content content content" */
 			"intro intro intro intro intro intro intro intro"
 			"about about about about about about about about"
 			"ft ft ft ft ft ft ft ft";
-	}
+	} */
 
 	/* L / Tablet Landscape */
-	@media only screen and (min-width: 992px) {
+	/* @media only screen and (min-width: 992px) {
 		grid-template-columns: repeat(10, 1fr);
 		grid-template-rows: auto 1fr 1fr auto;
-		/* margin: 57.5px 57.5px 0 57.5px; */
 		margin: 3.594rem 3.594rem 0 3.594rem;
-		/* margin: 0 3.594rem 0 3.594rem; */
 
 		grid-template-areas:
 			"hd hd hd hd hd hd hd hd hd hd"
-			/* "content content content content content content content content content content" */
 			"intro intro intro intro intro intro intro intro intro intro"
 			"about about about about about about about about about about"
 			"ft ft ft ft ft ft ft ft ft ft";
-	}
+	} */
 
 	/* XL / Desktop */
-	@media only screen and (min-width: 1200px) {
+	/* @media only screen and (min-width: 1200px) {
 		grid-template-columns: repeat(12, 1fr);
-		/* grid-template-rows: auto 1fr 1fr auto; */
 		grid-template-rows: auto auto auto auto;
 		grid-column-gap: 1.25rem;
-		/* margin: 0 70px 0 70px; */
-		/* margin: 0 4.375rem 0 4.375rem; */
 		margin: 4.375rem 4.375rem 0 4.375rem;
 
 		grid-template-areas:
 			"hd hd hd hd hd hd hd hd hd hd hd hd"
-			/* "content content content content content content content content content content content content" */
 			"intro intro intro intro intro intro intro intro intro intro intro intro"
 			"about about about about about about about about about about about about"
 			"ft ft ft ft ft ft ft ft ft ft ft ft";
-	}
+	} */
 `;
 
 // @media only screen and (min-width: 320px) {
--- src/Components/01-Header/style.js
+++ src/Components/01-Header/style.js
@@ -1,48 +1,28 @@
 import styled from "styled-components";
 
 export const HeaderCont = styled.div`
-	/* position: sticky; */
 	/* background-color: lightgray; */
-	grid-area: hd;
-	display: grid;
-	grid-template-columns: inherit;
+	/* grid-area: hd; */
+
+	/* display: grid; */
+	/* grid-template-columns: inherit;
 	grid-template-rows: 1;
 	grid-column-gap: 1.25rem;
 	grid-template-areas: "logo" "navbar";
 	text-align: center;
+	font-family: DM Serif Display; */
+	display: flex;
+	align-items: center;
+	justify-items: center;
+	justify-content: space-around;
 	font-family: DM Serif Display;
-
-	@media only screen and (min-width: 320px) {
-		ul {
-			grid-column-end: -1;
-		}
-
-		div {
-			grid-column-end: -1;
-		}
-	}
-
-	@media only screen and (min-width: 480px) {
-		ul {
-			grid-column-start: -5;
-		}
-	}
-
-	@media only screen and (min-width: 768px) {
-		ul {
-			grid-column-start: -6;
-		}
-	}
-
-	@media only screen and (min-width: 1200px) {
-		ul {
-			/* grid-column: 7 / span 6; */
-			grid-column-start: -7;
-		}
-	}
+	margin-top: 70px;
 `;
 
 export const Button = styled.button`
+	position: sticky;
+	top: 0;
+
 	font-family: "DM Serif Display";
 	cursor: pointer;
 	background-color: transparent;
@@ -51,53 +31,46 @@ export const Button = styled.button`
 	padding: 0;
 
 	@media only screen and (min-width: 320px) {
-		grid-column: 1 / span 2;
 		font-size: 2rem;
 	}
 
 	@media only screen and (min-width: 480px) {
-		grid-column: 1 / span 2;
 		font-size: 2.25rem;
 	}
 
 	@media only screen and (min-width: 768px) {
-		grid-column: 1 / span 3;
 		font-size: 2.5rem;
 	}
 
 	@media only screen and (min-width: 1200px) {
-		grid-column: 1 / span 4;
 		font-size: 3rem;
 	}
 `;
 
 export const NavBar = styled.ul`
+	position: sticky;
+	top: 0;
+
 	display: flex;
 	list-style: none;
 	padding: 0;
 	margin: 0;
-	justify-content: space-evenly;
-	align-self: center;
+	column-gap: 3rem;
 
 	@media only screen and (min-width: 320px) {
 		font-size: 1rem;
-		flex-direction: column;
 	}
 
 	@media only screen and (min-width: 480px) {
 		font-size: 1.25rem;
-		flex-direction: row;
 	}
 
 	@media only screen and (min-width: 768px) {
 		font-size: 1.25rem;
-		/* font-size: 1.5rem; */
-		flex-direction: row;
 	}
 
 	@media only screen and (min-width: 1200px) {
 		font-size: 1.5rem;
-		flex-direction: row;
 	}
 
 	a {

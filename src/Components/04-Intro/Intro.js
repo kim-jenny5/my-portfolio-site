8d221a5a6974788d13f2f7181d1e4b08b77a00c6
Jenny Kim <kimj1905@gmail.com>
2022-01-15 13:33:00 -0500

    aligned items to grid on intro section

--- src/Components/04-Intro/Intro.js
+++ src/Components/04-Intro/Intro.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { IntroCont, MainCont, TextCont } from "./style";
+import { IntroCont, MainCont, TextCont, Title, Subtitle, Jenny } from "./style";
+import jenny from "../../media/jenny.png";
 
 export default function Intro() {
 	return (
 		<IntroCont>
 			<MainCont>
-				<TextCont>Hi, my name is Jenny.</TextCont>
+				<TextCont>
+					<Title>
+						Hi, my name is Jenny.
+						<br />
+						Nice to meet you!
+					</Title>
+					<Subtitle>I'm a frontend developer.</Subtitle>
+				</TextCont>
+				<Jenny src={jenny}></Jenny>
 			</MainCont>
 		</IntroCont>
 	);
--- src/Components/04-Intro/style.js
+++ src/Components/04-Intro/style.js
@@ -5,18 +5,27 @@ export const IntroCont = styled.div`
 	background-color: lightpink;
 	display: grid;
 	grid-template-columns: inherit;
-	/* grid-template-rows: 5; */
-	/* grid-template-areas:
-		"top top top top top top top top top top top top"
-		"btm btm btm btm btm btm btm btm btm btm btm btm"; */
+	grid-column-gap: 1.25rem;
+	/* grid-template-rows: 0.25fr 0.5fr 0.25fr; */
+	grid-template-rows: auto 1fr auto;
+	/* grid-template-rows: 0.5fr 0.75fr 0.5fr; */
+	/* grid-template-rows: repeat(3, 1fr); */
+	grid-template-areas:
+		". . . . . . . . . . . ."
+		"container container container container container container container container container container container container"
+		". . . . . . . . . . . .";
+	align-items: center;
+	justify-items: center;
+	/* vertical-align: middle; */
 `;
 
 export const MainCont = styled.div`
-	/* grid-area:; */
+	grid-area: container;
 	display: grid;
 	grid-template-columns: repeat(10, 1fr);
 	grid-column-gap: 1.25rem;
-	grid-template-rows: 5;
+	/* grid-template-rows: 5; */
+	grid-template-rows: repeat(5, 1fr);
 	grid-template-areas:
 		". . . . . . img img img img"
 		"text text text text text . img img img img"
@@ -25,4 +34,31 @@ export const MainCont = styled.div`
 		". . . . . . img img img img";
 `;
 
-export const TextCont = styled.div``;
+export const TextCont = styled.div`
+	/* display: grid;
+	grid-template-columns: inherit;
+	grid-column-gap: inherit;
+	grid-template-rows: inherit; */
+	grid-area: text;
+	/* grid-column-start: 2; */
+	grid-column: 2 / span 7;
+	display: grid;
+	grid-template-rows: 1fr 0.5fr;
+`;
+
+export const Title = styled.div`
+	font-family: Corben;
+	font-weight: bold;
+	font-size: 36px;
+`;
+
+export const Subtitle = styled.div`
+	font-family: DM Serif Display;
+	font-size: 36px;
+`;
+
+export const Jenny = styled.img`
+	grid-area: img;
+	grid-column: -3;
+	width: 400px;
+`;

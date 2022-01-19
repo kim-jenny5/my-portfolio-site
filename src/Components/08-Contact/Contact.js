3cee14898127061a418c15c9df86c497ed7de55e
Jenny Kim <kimj1905@gmail.com>
2022-01-19 15:03:05 -0500

    basic set up of contact section finished:

--- src/Components/08-Contact/Contact.js
+++ src/Components/08-Contact/Contact.js
@@ -1,10 +1,34 @@
 import React from "react";
-import { ContactCont } from "./style";
+import { ContactCont, Jenny, ContactCard, TextCont, SNS } from "./style";
+import jenny from "../../media/jenny2.png";
+import { Envelope, GithubFill, LinkedInV1Fill } from "akar-icons";
 
 export default function Contact() {
 	return (
-		<ContactCont>
-			<div className="section-title">Contact Me</div>
+		<ContactCont id="contact">
+			<Jenny src={jenny}></Jenny>
+			<ContactCard>
+				<div>
+					<div className="bold">Jenny Kim</div>
+					<div>jennykimdev@gmail.com</div>
+				</div>
+				<div className="bold">Frontend Developer</div>
+				<SNS>
+					<a href="mailto:jennykimdev@gmail.com" target="_blank">
+						<Envelope strokeWidth={2} size={30} />
+					</a>
+					<a href="https://github.com/kim-jenny5" target="_blank">
+						<GithubFill strokeWidth={2} size={30} />
+					</a>
+					<a href="https://www.linkedin.com/in/kim-jenny5/" target="_blank">
+						<LinkedInV1Fill strokeWidth={2} size={30} />
+					</a>
+				</SNS>
+				<div>
+					<div>City</div>
+					<div className="bold">New York, NY</div>
+				</div>
+			</ContactCard>
 		</ContactCont>
 	);
 }
--- src/Components/08-Contact/style.js
+++ src/Components/08-Contact/style.js
@@ -1,6 +1,65 @@
 import styled from "styled-components";
 
 export const ContactCont = styled.div`
-	background-color: lightsteelblue;
+	/* background-color: lightsteelblue; */
 	padding-bottom: 100px;
+	z-index: -2;
+
+	display: flex;
+	flex-direction: column;
+`;
+
+// export const TextCont = styled.div`
+// 	display: flex;
+// 	flex-direction: column;
+// 	/* font-size: 1.125rem; */
+// 	font-size: 1.25rem;
+
+// 	> div:first-child {
+// 	}
+// `;
+
+export const Jenny = styled.img`
+	width: 200px;
+	z-index: 1;
+	align-self: center;
+`;
+
+export const SNS = styled.div`
+	display: flex;
+	flex-direction: row;
+	column-gap: 40px;
+	align-self: center;
+
+	a {
+		text-decoration: none;
+		color: #537962;
+	}
+`;
+
+export const ContactCard = styled.div`
+	/* border: 1px solid #f5f3f6; */
+	border: 1px solid #eae3d7;
+	border-radius: 15px;
+	box-shadow: 3px 3px 6px #e0e0e0;
+	/* box-shadow: inset 3em 3em #eae3d7; */
+	width: 40%;
+	align-self: center;
+	text-align: center;
+
+	margin-top: -100px;
+	padding-top: 125px;
+	padding-bottom: 30px;
+
+	display: flex;
+	flex-direction: column;
+	row-gap: 20px;
+
+	> div {
+		z-index: -1;
+	}
+
+	.bold {
+		font-weight: bold;
+	}
 `;

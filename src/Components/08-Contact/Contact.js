e898975af71dedbcc00b6c1305eb9fa2dc9d535f
Jenny Kim <kimj1905@gmail.com>
2022-01-19 15:23:53 -0500

    added feather icons react pkg

--- src/Components/08-Contact/Contact.js
+++ src/Components/08-Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ContactCont, Jenny, ContactCard, TextCont, SNS } from "./style";
 import jenny from "../../media/jenny2.png";
+import { AtSign, Linkedin, GitHub } from "react-feather";
 
 export default function Contact() {
 	return (
@@ -15,12 +16,15 @@ export default function Contact() {
 				<SNS>
 					<a href="mailto:jennykimdev@gmail.com" target="_blank">
 						{/* <Envelope strokeWidth={2} size={30} /> */}
+						<AtSign />
 					</a>
 					<a href="https://github.com/kim-jenny5" target="_blank">
 						{/* <GithubFill strokeWidth={2} size={30} /> */}
+						<GitHub />
 					</a>
 					<a href="https://www.linkedin.com/in/kim-jenny5/" target="_blank">
 						{/* <LinkedInV1Fill strokeWidth={2} size={30} /> */}
+						<Linkedin />
 					</a>
 				</SNS>
 				<div>

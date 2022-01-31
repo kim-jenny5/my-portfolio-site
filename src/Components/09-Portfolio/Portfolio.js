d149c5d77e33b8b563bf67a032a3bc294ba5cab2
Jenny Kim <kimj1905@gmail.com>
2022-01-31 15:43:20 -0500

    small change made

--- src/Components/09-Portfolio/Portfolio.js
+++ src/Components/09-Portfolio/Portfolio.js
@@ -24,7 +24,7 @@ export default function Portfolio() {
 						</a>
 					</span>
 				</div>
-				<div>for this portfolio</div>
+				<div>for this portfolio site</div>
 			</SemiCircle>
 		</PortfolioCont>
 	);

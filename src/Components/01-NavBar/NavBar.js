2cca147c9e97aa66ff726f687531b2ef70d9e9a2
Jenny Kim <kimj1905@gmail.com>
2024-06-21 00:36:57 -0400

    start on nav

--- src/Components/01-NavBar/NavBar.js
+++ src/Components/01-NavBar/NavBar.js
@@ -1,13 +1,27 @@
-import React from "react";
-// import { StyledLink } from "./style";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
 	return (
-		<nav>
+		<nav className='flex justify-between items-center p-5'>
 			<div>
-				<Link to="/" className="uppercase font-medium">
-					jenny kim
+				<Link
+					to='/'
+					className='inline-block uppercase font-medium text-orion text-5xl'
+				>
+					Jenny Kim
+				</Link>
+				<div className='inline-block rounded-full w-3 h-3 bg-obsidian ml-1'></div>
+			</div>
+			<div className='flex gap-x-3'>
+				<Link to='/' className='uppercase font-medium text-orion text-xl'>
+					About
+				</Link>
+				<Link to='/' className='uppercase font-medium text-orion text-xl'>
+					Projects
+				</Link>
+				<Link to='/' className='uppercase font-medium text-orion text-xl'>
+					Contact
 				</Link>
 			</div>
 		</nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				orion: '#1D5AE9',
+				moss: '#BABE6D',
+				obsidian: '#1C1D21',
+				grape: '#742DDF',
+				mushroom: '#A59980',
+				cream: '#E6E4DE'
+			}
+		}
 	},
 	plugins: []
 };

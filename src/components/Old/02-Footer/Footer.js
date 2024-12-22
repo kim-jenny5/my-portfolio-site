4b1b4a4ea87d67a4e5e88a26569274804b67a163
Jenny Kim <kimj1905@gmail.com>
2024-12-21 21:29:09 -0500

    remove unused pckgs and update readme

--- src/components/Old/02-Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { FooterCont } from "./style";
-
-export default function Footer() {
-	const thisYear = new Date().getFullYear();
-
-	return <FooterCont>© {thisYear} JENNY KIM</FooterCont>;
-}

29b849e9f9c16c08bde98f4aed234d6f38376756
Jenny Kim <kimj1905@gmail.com>
2025-07-31 11:03:24 -0400

    add the great estimation

--- src/components/Projects/Content.js
+++ src/components/Projects/Content.js
@@ -3,18 +3,10 @@ import macOSFolder from '../../assets/images/macos_folder.png';
 import PROJECTS from './Data';
 
 export default function Content({ selectProject, selectedProject }) {
-	const renderSelectedProject = () => {
-		if (!selectedProject) return null;
-
-		switch (selectedProject.name) {
-			case 'BDG Media':
-			case 'Personal':
-			case 'Student':
-				return <GalleryWrapper projects={selectedProject.projects} />;
-			default:
-				return null;
-		}
-	};
+	const renderSelectedProject = () =>
+		selectedProject ? (
+			<GalleryWrapper projects={selectedProject.projects} />
+		) : null;
 
 	return (
 		<>
@@ -22,28 +14,29 @@ export default function Content({ selectProject, selectedProject }) {
 				renderSelectedProject()
 			) : (
 				<div className='flex flex-col gap-y-4'>
-					{PROJECTS.map((project, index) => (
+					{PROJECTS.map((group, index) => (
 						<div key={index}>
 							<div className='flex justify-between items-center px-6 py-2 text-gray-500'>
-								<span className='font-medium tracking-wide'>
-									{project.year}
-								</span>
+								<span className='font-medium tracking-wide'>{group.label}</span>
 								<span className='font-light text-xs'>Show Less</span>
 							</div>
-							<div className='border-t border-gray-200 px-6 py-2 select-none'>
-								<div
-									tabIndex={0}
-									className='inline-block text-center cursor-pointer group'
-									onClick={() => selectProject(project)}
-								>
-									<img
-										src={macOSFolder}
-										className='w-[100px] p-1 rounded-md group-active:bg-gray-200'
-									/>
-									<div className='text-sm inline-block px-1 rounded group-active:bg-blue-600 group-active:text-white'>
-										{project.name}
+							<div className='border-t border-gray-200 px-6 py-2 select-none flex gap-x-4'>
+								{group.projects.map((subgroup, i) => (
+									<div
+										key={i}
+										tabIndex={0}
+										className='text-center cursor-pointer group'
+										onClick={() => selectProject(subgroup)}
+									>
+										<img
+											src={macOSFolder}
+											className='w-[100px] p-1 rounded-md group-active:bg-gray-200'
+										/>
+										<div className='text-sm inline-block px-1 rounded group-active:bg-blue-600 group-active:text-white'>
+											{subgroup.name}
+										</div>
 									</div>
-								</div>
+								))}
 							</div>
 						</div>
 					))}
--- src/components/Projects/Data.js
+++ src/components/Projects/Data.js
@@ -1,6 +1,6 @@
+import the_great_estimation from '../../assets/projects/the_great_estimation.png';
 import ten_minutes_planner from '../../assets/projects/ten_minutes_planner.png';
 import portfolio_site from '../../assets/projects/portfolio_site.png';
-import friend_portfolio_site_prototype from '../../assets/projects/friend_portfolio_site_prototype.png';
 import portfolio_site_v1 from '../../assets/projects/portfolio_site_v1.png';
 import bustle_newsletter from '../../assets/projects/bustle_newsletter.png';
 import pilim from '../../assets/projects/pilim.png';
@@ -11,140 +11,153 @@ import tableau_dambiance from '../../assets/projects/tableau_dambiance.png';
 // hard coded for now
 const PROJECTS = [
 	{
-		year: '2022-2025',
-		name: 'Personal',
+		label: 'Personal',
 		projects: [
 			{
-				name: '10 Minutes Planner (Motemote Digital Clone)',
-				img: ten_minutes_planner,
-				createdAt: 'July 14, 2025',
-				active: true,
-				tech: 'Next.js, React, TypeScript, Motion, HTML, CSS (Tailwind CSS), Supabase',
-				links: [
+				name: 'Featured',
+				projects: [
 					{
-						gitHub: ['https://github.com/kim-jenny5/motemote-10m-planner'],
-						live: 'https://motemote-10m-planner.vercel.app/',
-					},
-				],
-			},
-			{
-				name: 'Portfolio Website',
-				img: portfolio_site,
-				createdAt: 'June 19, 2024',
-				active: true,
-				tech: 'React, JavaScript, Motion, HTML, CSS (Tailwind CSS)',
-				links: [
-					{
-						gitHub: ['https://github.com/kim-jenny5/my-portfolio-site'],
-						live: 'https://jennykim.dev/',
-					},
-				],
-			},
-			{
-				name: "Friend's Portfolio Site Prototype",
-				img: friend_portfolio_site_prototype,
-				createdAt: 'March 12, 2022',
-				active: false,
-				tech: 'React, JavaScript, HTML, CSS (Styled Components), Ruby on Rails',
-				links: [
-					{
-						gitHub: [
-							'https://github.com/kim-jenny5/moon-portfolio-frontend',
-							'https://github.com/kim-jenny5/moon-portfolio-backend',
+						name: 'The Great Estimation',
+						img: the_great_estimation,
+						createdAt: 'July 21, 2025',
+						active: true,
+						tech: 'Next.js, React, TypeScript, HTML, CSS (Tailwind CSS & Radix UI), Neon, Drizzle',
+						links: [
+							{
+								gitHub: ['https://github.com/kim-jenny5/the-great-estimation'],
+								live: 'https://the-great-estimation.vercel.app/',
+							},
 						],
-						live: 'https://romantic-kirch-86d465.netlify.app/',
+					},
+					{
+						name: '10 Minutes Planner (Motemote Digital Clone)',
+						img: ten_minutes_planner,
+						createdAt: 'July 14, 2025',
+						active: true,
+						tech: 'Next.js, React, TypeScript, Motion, HTML, CSS (Tailwind CSS), Supabase',
+						links: [
+							{
+								gitHub: ['https://github.com/kim-jenny5/motemote-10m-planner'],
+								live: 'https://motemote-10m-planner.vercel.app/',
+							},
+						],
 					},
 				],
 			},
 			{
-				name: 'Portfolio Website v1',
-				img: portfolio_site_v1,
-				createdAt: 'January 7, 2022',
-				active: true,
-				tech: 'React, JavaScript, HTML, CSS (Styled Components)',
-				links: [
+				name: 'Website',
+				projects: [
 					{
-						gitHub: ['https://github.com/kim-jenny5/my-portfolio-site-v1'],
-						live: 'https://portfolio-site-v1.netlify.app/',
+						name: 'Portfolio Website',
+						img: portfolio_site,
+						createdAt: 'June 19, 2024',
+						active: true,
+						tech: 'React, JavaScript, Motion, HTML, CSS (Tailwind CSS)',
+						links: [
+							{
+								gitHub: ['https://github.com/kim-jenny5/my-portfolio-site'],
+								live: 'https://jennykim.dev/',
+							},
+						],
+					},
+					{
+						name: 'Portfolio Website v1',
+						img: portfolio_site_v1,
+						createdAt: 'January 7, 2022',
+						active: true,
+						tech: 'React, JavaScript, HTML, CSS (Styled Components)',
+						links: [
+							{
+								gitHub: ['https://github.com/kim-jenny5/my-portfolio-site-v1'],
+								live: 'https://portfolio-site-v1.netlify.app/',
+							},
+						],
+					},
+				],
+			},
+			{
+				name: 'Student',
+				projects: [
+					{
+						name: 'my travelogue',
+						img: my_travelogue,
+						createdAt: 'January 5, 2022',
+						active: false,
+						tech: 'React, JavaScript, HTML, CSS (Material UI), Ruby on Rails, PostgreSQL, JWT',
+						links: [
+							{
+								gitHub: [
+									'https://github.com/kim-jenny5/my-travelogue-frontend',
+									'https://github.com/kim-jenny5/my-travelogue-backend',
+								],
+							},
+							{ live: 'https://my-travelogue.netlify.app/' },
+						],
+					},
+					{
+						name: "Tableau d'Ambiance",
+						img: tableau_dambiance,
+						createdAt: 'September 27, 2021',
+						active: false,
+						tech: 'JavaScript, HTML, CSS (Bootstrap), Ruby on Rails, PostgreSQL, Active Storage, AWS S3',
+						links: [
+							{
+								gitHub: [
+									'https://github.com/kim-jenny5/tableau-dambiance-frontend',
+									'https://github.com/kim-jenny5/tableau-dambiance-backend',
+								],
+							},
+							{ live: 'https://pilim-project.herokuapp.com/' },
+						],
+					},
+					{
+						name: 'Pilim',
+						img: pilim,
+						createdAt: 'August 3, 2021',
+						active: false,
+						tech: 'JavaScript, Ruby on Rails, HTML, CSS (Bootstrap)',
+						links: [
+							{ gitHub: ['https://github.com/kim-jenny5/pilim-project'] },
+							{ live: 'https://pilim-project.herokuapp.com/' },
+						],
+					},
+					{
+						name: 'My Daily Journal',
+						img: my_daily_journal,
+						createdAt: 'June 6, 2021',
+						active: false,
+						tech: 'HTML, CSS, Ruby, Sinatra',
+						links: [
+							{
+								gitHub: [
+									'https://github.com/kim-jenny5/journal-sinatra-project',
+								],
+							},
+							{ live: 'https://mydailyjournal-byjenny.herokuapp.com/' },
+						],
 					},
 				],
 			},
 		],
 	},
 	{
-		year: '2022-2025',
-		name: 'BDG Media',
+		label: 'Professional',
 		projects: [
 			{
-				name: 'Email Newsletters',
-				img: bustle_newsletter,
-				createdAt: '2023',
-				active: true,
-				tech: 'HTML, CSS',
-				links: [
+				name: 'BDG Media',
+				projects: [
 					{
-						live: 'https://app.sparkmailapp.com/web-share/APC34KaAnmq-ZCSWzF00E67nQ8QPQ1BavKTVuWNL',
-					},
-				],
-			},
-		],
-	},
-	{
-		year: '2021',
-		name: 'Student',
-		projects: [
-			{
-				name: 'my travelogue',
-				img: my_travelogue,
-				createdAt: 'January 5, 2022',
-				active: false,
-				tech: 'React, JavaScript, HTML, CSS (Material UI), Ruby on Rails, PostgreSQL, JWT',
-				links: [
-					{
-						gitHub: [
-							'https://github.com/kim-jenny5/my-travelogue-frontend',
-							'https://github.com/kim-jenny5/my-travelogue-backend',
+						name: 'Email Newsletters',
+						img: bustle_newsletter,
+						createdAt: '2023',
+						active: true,
+						tech: 'HTML, CSS',
+						links: [
+							{
+								live: 'https://app.sparkmailapp.com/web-share/APC34KaAnmq-ZCSWzF00E67nQ8QPQ1BavKTVuWNL',
+							},
 						],
 					},
-					{ live: 'https://my-travelogue.netlify.app/' },
-				],
-			},
-			{
-				name: "Tableau d'Ambiance",
-				img: tableau_dambiance,
-				createdAt: 'September 27, 2021',
-				active: false,
-				tech: 'JavaScript, HTML, CSS (Bootstrap), Ruby on Rails, PostgreSQL, Active Storage, AWS S3',
-				links: [
-					{
-						gitHub: [
-							'https://github.com/kim-jenny5/tableau-dambiance-frontend',
-							'https://github.com/kim-jenny5/tableau-dambiance-backend',
-						],
-					},
-					{ live: 'https://pilim-project.herokuapp.com/' },
-				],
-			},
-			{
-				name: 'Pilim',
-				img: pilim,
-				createdAt: 'August 3, 2021',
-				active: false,
-				tech: 'JavaScript, Ruby on Rails, HTML, CSS (Bootstrap)',
-				links: [
-					{ gitHub: ['https://github.com/kim-jenny5/pilim-project'] },
-					{ live: 'https://pilim-project.herokuapp.com/' },
-				],
-			},
-			{
-				name: 'My Daily Journal',
-				img: my_daily_journal,
-				createdAt: 'June 6, 2021',
-				active: false,
-				tech: 'HTML, CSS, Ruby, Sinatra',
-				links: [
-					{ gitHub: ['https://github.com/kim-jenny5/journal-sinatra-project'] },
-					{ live: 'https://mydailyjournal-byjenny.herokuapp.com/' },
 				],
 			},
 		],
